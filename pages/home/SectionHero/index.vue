a809e40a353a739a3b01bbe3553a693be4d2ebe9
Ivo Dolenc <ivodolenc@gmail.com>
2022-06-25 15:58:23 +0200

    refactor: update `class` names

--- pages/home/SectionHero/index.vue
+++ pages/home/SectionHero/index.vue
@@ -1,52 +1,21 @@
 <template>
   <div
     data-section-hero
-    class="
-      fixed
-      z-50
-      top-0
-      left-0
-      w-full
-      h-full
-      flex
-      items-center
-      justify-center
-      pointer-events-none
-      p-10
-    "
+    class="fixed z-50 top-0 left-0 w-full h-full flex items-center justify-center pointer-events-none p-10"
   >
-    <div class="z-30 w-full max-w-screen-[60vw] lg:max-w-screen-[40vw]">
+    <div class="z-30 w-full max-w-[60vw] lg:max-w-[40vw]">
       <figure class="relative block pt-[100%]">
         <img
           data-img-star
           data-src="/images/star.png"
-          class="
-            lazyload
-            absolute
-            top-0
-            left-0
-            w-full
-            h-full
-            object-cover
-            opacity-0
-            transform
-            scale-0
-          "
+          class="lazyload absolute top-0 left-0 w-full h-full object-cover opacity-0 transform scale-0"
           alt=""
           draggable="false"
         />
       </figure>
     </div>
     <div
-      class="
-        z-10
-        absolute
-        w-full
-        max-w-screen-xl
-        xl:max-w-screen-[80vw]
-        overflow-hidden
-        p-10
-      "
+      class="z-10 absolute w-full max-w-screen-xl xl:max-w-[80vw] overflow-hidden p-10"
     >
       <SvgTitle data-svg-title class="opacity-0 transform translate-y-[150%]" />
     </div>
